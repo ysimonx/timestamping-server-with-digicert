84316cd342aa44785030792a8a2a6f488c27f7b9
Yannick Simon <yannick.simon@gmail.com>
2023-03-04 16:40:49 +0100

    init

---
+++ run.sh
@@ -0,0 +1,28 @@
+# cf https://knowledge.digicert.com/generalinformation/INFO4231.html
+# cf https://www.binalyze.com/blog/dfir-lab/protect-your-chain-of-custody-with-content-hashing-and-timestamping
+# cf python https://github.com/pyauth/tsp-client qui indique que digicert n''est pas https ...'
+echo "test" > file.txt
+openssl ts -query -data file.txt -no_nonce -sha512 -cert -out file.digicert.tsq
+curl -H "Content-Type: application/timestamp-query" --data-binary '@file.digicert.tsq'  https://timestamp.digicert.com > file.digicert.tsr
+
+
+
+echo "timestamp token is"
+openssl ts -reply -in file.digicert.tsr -text
+
+sleep 10
+
+echo 
+echo "processing verification ..."
+echo 
+rm *.cer
+wget https://knowledge.digicert.com/content/dam/digicertknowledgebase/attachments/time-stamp/TSACertificate.cer
+wget https://knowledge.digicert.com/content/dam/digicertknowledgebase/DigiCertTrustedG4RSA4096SHA256TimeStampingCA.cer
+wget https://knowledge.digicert.com/content/dam/digicertknowledgebase/DigiCertTrustedRootG4.cer
+
+cat TSACertificate.cer > CHAIN.pem 
+cat DigiCertTrustedG4RSA4096SHA256TimeStampingCA.cer >> CHAIN.pem
+cat DigiCertTrustedRootG4.cer >> CHAIN.pem
+
+
+openssl ts -verify -in file.digicert.tsr -queryfile file.digicert.tsq -CAfile DigiCertAssuredIDRootCA.crt.pem -untrusted CHAIN.pem
