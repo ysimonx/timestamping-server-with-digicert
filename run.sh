6f5c3f7635ef2826d09078e33fe4233bde0cfcaf
Yannick Simon <yannick.simon@gmail.com>
2023-03-06 08:22:46 +0100

    upd

--- run.sh
+++ run.sh
@@ -1,12 +1,18 @@
 # cf https://knowledge.digicert.com/generalinformation/INFO4231.html
+# cf https://www.openssl.org/docs/man1.1.1/man1/openssl-ts.html
 # cf https://www.binalyze.com/blog/dfir-lab/protect-your-chain-of-custody-with-content-hashing-and-timestamping
 # cf python https://github.com/pyauth/tsp-client qui indique que digicert n''est pas https ...'
 echo "test" > file.txt
+
+# in one line : file.txt -> sha512 -> build query -> file.digicert.tsq
 openssl ts -query -data file.txt -no_nonce -sha512 -cert -out file.digicert.tsq
+
+echo "request query is : "
+openssl ts -query -in file.digicert.tsq -text
+sleep 5
+
+
 curl -H "Content-Type: application/timestamp-query" --data-binary '@file.digicert.tsq'  http://timestamp.digicert.com > file.digicert.tsr
-
-
-
 echo "timestamp token is"
 openssl ts -reply -in file.digicert.tsr -text
 
@@ -15,6 +21,9 @@ sleep 10
 echo 
 echo "processing verification ..."
 echo 
+sleep
+
+echo "downloading digicert's public certificates"
 rm *.cer
 wget https://knowledge.digicert.com/content/dam/digicertknowledgebase/attachments/time-stamp/TSACertificate.cer
 wget https://knowledge.digicert.com/content/dam/digicertknowledgebase/DigiCertTrustedG4RSA4096SHA256TimeStampingCA.cer
@@ -23,6 +32,11 @@ wget https://knowledge.digicert.com/content/dam/digicertknowledgebase/DigiCertTr
 cat TSACertificate.cer > CHAIN.pem 
 cat DigiCertTrustedG4RSA4096SHA256TimeStampingCA.cer >> CHAIN.pem
 cat DigiCertTrustedRootG4.cer >> CHAIN.pem
+echo
+echo
+echo "verifying previous response with digicert's public certificates"
+sleep 5
+echo
 
 
 openssl ts -verify -in file.digicert.tsr -queryfile file.digicert.tsq -CAfile DigiCertAssuredIDRootCA.crt.pem -untrusted CHAIN.pem
